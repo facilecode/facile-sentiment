41a1cecf55eec38044059800171f8b5351340d04
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-25 15:55:31 +0100

    vuetify added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- <v-app-bar app color="primary" dark>
+    </v-app-bar> -->
+
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col >
+          <v-card class="mx-auto" max-width="400" >
+              <v-img src="./assets/pt-logo.png"></v-img>
+
+              <v-card-title>
+                  Title
+              </v-card-title>
+              <v-card-subtitle>
+                  Subtitle
+              </v-card-subtitle>
+
+              <v-card-actions>
+              <v-btn color="orange lighten-2" text > Explore </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="orange lighten-2" text >Load Image</v-btn>
+              </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- input fields -->
+        <v-col>
+        </v-col>
+        <!-- end-filter -->
+        <v-col>
+          <v-card class="mx-auto" max-width="400">
+            <v-img src="./assets/tf.png"></v-img>
+
+            <v-card-title>
+                Title
+            </v-card-title>
+            <v-card-subtitle>
+                Subtitle
+            </v-card-subtitle>
+
+            <v-card-actions>
+            <v-btn color="orange lighten-2" text > Explore </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="orange lighten-2" text >Load Image</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Main from './views/Main.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+  },
+
+  data: () => ({
+    //
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+    </v-app-bar>
+
+    <v-container>
+    </v-container>
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
